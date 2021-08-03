5e57aeb3bb5bedda35a3290a513d17a6d3776bee
DamianLtz <77624662+DamianLtz@users.noreply.github.com>
2021-08-02 23:05:50 -0300

    v0.4

---
+++ js/mainAnimaciones.js
@@ -0,0 +1,28 @@
+$('.dropdown-menu').addClass('invisible'); //FIRST TIME INVISIBLE
+
+// ADD SLIDEDOWN ANIMATION TO DROPDOWN-MENU
+$('.dropdown').on('show.bs.dropdown', function (e) {
+    $('.dropdown-menu').removeClass('invisible');
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown(250);
+});
+
+// ADD SLIDEUP ANIMATION TO DROPDOWN-MENU
+$('.dropdown').on('hide.bs.dropdown', function (e) {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp(250);
+});
+
+$(function () {
+    $(".dropdown").hover(
+        function () {
+            $('.dropdown-menu', this).stop(true, true).fadeIn("fast");
+            $(this).toggleClass('open');
+            $('b', this).toggleClass("caret caret-up");
+        },
+        function () {
+            $('.dropdown-menu', this).stop(true, true).fadeOut("fast");
+            $(this).toggleClass('open');
+            $('b', this).toggleClass("caret caret-up");
+        });
+});
+
+const TOUCHEVENT_COMPAT_WAIT = 50; // Time for mouse compat events to fire after touch
